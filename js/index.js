6ec853a6102f36e7f1b54cba2a6e04199da509d3
Zharkynbek Idirisov <zh.idirisov@gmail.com>
2021-03-05 23:22:55 +0100

    rendering and add function open/close modal

---
+++ js/index.js
@@ -0,0 +1,52 @@
+import images from './gallery-items.js'
+
+const ulRef = document.querySelector('.js-gallery')
+const modalOpenRef = document.querySelector('.js-lightbox')
+const modalImgRef = document.querySelector('.lightbox__image')
+const closeModalRef = document.querySelector('.lightbox__button')
+
+const galleryRef = images.map(({ preview, original, description }) => {
+    
+    return `<li class="gallery__item"><a class="gallery__link"><img class="gallery__image" src="${preview}" ${original} alt="${description}></a></li>`
+})
+
+ulRef.insertAdjacentHTML('beforeend', galleryRef)
+
+// Open/Close function
+
+ulRef.addEventListener('click', onOpenModal)
+closeModalRef.addEventListener('click', onCloseModal)
+window.addEventListener('click', closeModalOverlay)
+window.addEventListener('keyup', onCloseModalRef)
+
+
+// Open Modal
+
+function onOpenModal(e) {
+    if (e.target.tagName === 'IMG') {
+        modalOpenRef.classList.add('is-open')
+        modalImgRef.src = e.target.src
+        modalImgRef.alt = e.target.alt
+    }   
+}
+// Close with Click on Image
+
+function onCloseModal() {
+     modalOpenRef.classList.remove('is-open')
+}
+
+// Close with Click on Overlay
+
+function closeModalOverlay(e) {
+    if (e.target.tagName !== 'IMG') {
+        modalOpenRef.classList.remove('is-open')
+    }   
+}
+
+// Close with Escape
+
+function onCloseModalRef(event) {
+    if (event.key === 'Escape') {
+        modalOpenRef.classList.remove('is-open')
+    }   
+}
